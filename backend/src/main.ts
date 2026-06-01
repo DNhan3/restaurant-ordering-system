3795204a24f50e883d7baec87a46d2e16048a217
DNhan3 <96424205+DNhan3@users.noreply.github.com>
2026-06-01 21:53:21 +0700

    feat: update CORS configuration to use environment variable for frontend URL

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,8 @@ import { join } from 'node:path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
   });
   app.use('/uploads', express.static(join(process.cwd(), 'uploads')));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
